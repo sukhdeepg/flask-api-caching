d72bf675a179beefa1dbb83d2fef2485597ea220
gill <gillsukhdeep452@gmail.com>
2022-06-15 22:18:18 +0530

    added Nobel Prize API

--- app.py
+++ app.py
@@ -2,3 +2,14 @@ from flask import Flask, request, jsonify
 import requests
 
 app = Flask(__name__)
+
+@app.route("/nobelprize")
+def get_universities():
+    category = request.args.get("category")
+    year = request.args.get("year")
+    r = requests.get(f"https://api.nobelprize.org/2.1/nobelPrize/{category}/{year}")
+    return jsonify(r.json())
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
