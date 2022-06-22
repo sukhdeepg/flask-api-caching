31e4f58a5e6eca4b4071cfe7e32b68d663969bdb
gill <gillsukhdeep452@gmail.com>
2022-06-22 21:38:14 +0530

    added files related to Docker, redis integration

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask, request, jsonify
+from flask_caching import Cache
 import requests
 
 app = Flask(__name__)
+app.config.from_object('config.BaseConfig')
+cache = Cache(app)
 
 @app.route("/nobelprize")
+@cache.cached(timeout=30, query_string=True)
 def get_universities():
     category = request.args.get("category")
     year = request.args.get("year")
     r = requests.get(f"https://api.nobelprize.org/2.1/nobelPrize/{category}/{year}")
     return jsonify(r.json())
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
